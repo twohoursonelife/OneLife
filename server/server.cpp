90a2db3b4822d160268d2af85995a7777a13729d
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-08-28 11:54:52 -0700

    Can spend YUM bonus to do hungry work.  Hungry worked blocked if it would take you down below 5 food (to give you a safe buffer at the end).  Thus, you need at least 15 food (including food and YUM) to do 10-cost hungry work.

--- server/server.cpp
+++ server/server.cpp
@@ -17821,19 +17821,24 @@ int main() {
                                         }
                                     
                                     if( hungryWorkCost > 0 ) {
-                                        int oldStore = nextPlayer->foodStore;
+                                        if( nextPlayer->yummyBonusStore > 0 ) {
+                                            if( nextPlayer->yummyBonusStore
+                                                >= hungryWorkCost ) {
+                                                nextPlayer->yummyBonusStore -=
+                                                    hungryWorkCost;
+                                                hungryWorkCost = 0;
+                                                }
+                                            else {
+                                                hungryWorkCost -= 
+                                                    nextPlayer->yummyBonusStore;
+                                                nextPlayer->yummyBonusStore = 0;
+                                                }
+                                            }
                                         
                                         nextPlayer->foodStore -= hungryWorkCost;
                                         
-                                        if( nextPlayer->foodStore < 3 ) {
-                                            if( oldStore > 3  ) {
-                                                // generally leave
-                                                // player with 3 food
-                                                // unless they had less than
-                                                // 3 to start
-                                                nextPlayer->foodStore = 3;
-                                                }
-                                            }
+                                        // we checked above, so player
+                                        // never is taken down below 5 here
                                         nextPlayer->foodUpdate = true;
                                         }
                                     
