78f8bf9f557589452875fd3fdcd4fafa88d384a7
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-12-05 15:07:24 -0800

    Fixed a memory leak.

--- gameSource/spriteBank.cpp
+++ gameSource/spriteBank.cpp
@@ -24,7 +24,7 @@ static int mapSize;
 // maps IDs to records
 // sparse, so some entries are NULL
 static SpriteRecord **idMap;
-static char *spriteDrawnMap;
+static char *spriteDrawnMap = NULL;
 
 
 static StringTree tree;
@@ -618,6 +618,9 @@ void freeSpriteBank() {
     if( blankSprite != NULL ) {
         freeSprite( blankSprite );
         }
+
+    delete [] spriteDrawnMap;
+
 
     spriteBankLoaded = false;
     }
