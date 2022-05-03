a8d0eb8bc0a3a0576a1cd5f07951ac915f427060
risvh <67486979+risvh@users.noreply.github.com>
2022-05-03 23:39:02 +0800

    Revert "separate name and tag on client side"
    This reverts commit 767d5b858864dfe7090c5d4468c2d0c6144a75c8.

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -2862,10 +2862,6 @@ void LivingLifePage::clearLiveObjects() {
         if( nextObject->name != NULL ) {
             delete [] nextObject->name;
             }
-            
-        if( nextObject->tag != NULL ) {
-            delete [] nextObject->tag;
-            }
 
         delete nextObject->futureAnimStack;
         delete nextObject->futureHeldAnimStack;
@@ -9174,25 +9170,9 @@ void LivingLifePage::draw( doublePair inViewCenter,
                     }
                 else {
                     des = (char*)translate( "you" );
-                    if( ourLiveObject->name != NULL || ourLiveObject->tag != NULL ) {
-                        
-                        char* displayName;
-                        if( ourLiveObject->name != NULL && ourLiveObject->tag != NULL ) {
-                            displayName = autoSprintf( "%s %s",
-                                               ourLiveObject->name, 
-                                               ourLiveObject->tag );
-                            }
-                        else if( ourLiveObject->name != NULL ) {
-                            displayName = autoSprintf( "%s",
-                                               ourLiveObject->name );
-                            }
-                        else if( ourLiveObject->tag != NULL ) {
-                            displayName = autoSprintf( "%s",
-                                               ourLiveObject->tag );
-                            }
-
+                    if( ourLiveObject->name != NULL ) {
                         des = autoSprintf( "%s - %s", des, 
-                                           displayName );
+                                           ourLiveObject->name );
                         desToDelete = des;
                         }
                     }
@@ -9206,27 +9186,9 @@ void LivingLifePage::draw( doublePair inViewCenter,
                 if( des == NULL ) {
                     des = (char*)translate( "unrelated" );
                     }
-                if( otherObj != NULL && 
-                    ( otherObj->name != NULL || otherObj->tag != NULL )
-                    ) {
-                        
-                    char* displayName;
-                    if( otherObj->name != NULL && otherObj->tag != NULL ) {
-                        displayName = autoSprintf( "%s %s",
-                                           otherObj->name, 
-                                           otherObj->tag );
-                        }
-                    else if( otherObj->name != NULL ) {
-                        displayName = autoSprintf( "%s",
-                                           otherObj->name );
-                        }
-                    else if( otherObj->tag != NULL ) {
-                        displayName = autoSprintf( "%s",
-                                           otherObj->tag );
-                        }
-                        
+                if( otherObj != NULL && otherObj->name != NULL ) {
                     des = autoSprintf( "%s - %s",
-                                       displayName, des );
+                                       otherObj->name, des );
                     desToDelete = des;
                     }
                 if( otherObj != NULL && 
@@ -14329,7 +14291,6 @@ void LivingLifePage::step() {
 
                 o.name = NULL;
                 o.relationName = NULL;
-                o.tag = NULL;
 
                 o.curseLevel = 0;
                 o.excessCursePoints = 0;
@@ -17527,34 +17488,16 @@ void LivingLifePage::step() {
                             if( existing->name != NULL ) {
                                 delete [] existing->name;
                                 }
-                                
-                            if( existing->tag != NULL ) {
-                                delete [] existing->tag;
-                                existing->tag = NULL;
-                                }
                             
                             char *firstSpace = strstr( lines[i], " " );
         
                             if( firstSpace != NULL ) {
 
-                                
-                                char *firstPlus = strstr( lines[i], "+" );
-                                
-                                if( firstPlus != NULL ) {
-                                    char *tagStart = &( firstPlus[0] );
-                                    existing->tag = stringDuplicate( tagStart );
-                                    (firstPlus - 1)[0] = '\0';
-                                    }
-
                                 char *nameStart = &( firstSpace[1] );
                                 
-                                if( firstSpace[1] != '+' ) {
-                                
-                                    existing->name = stringDuplicate( nameStart );
-                                    
-                                    }
-                                    
-                                LiveObject *ourLiveObject = getOurLiveObject();
+                                existing->name = stringDuplicate( nameStart );
+								
+								LiveObject *ourLiveObject = getOurLiveObject();
 								if ( id == ourLiveObject->id && 
 									//Little hack here to not have the ding
 									//when we are just reconnected
