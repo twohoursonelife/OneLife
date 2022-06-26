6b4203ce793bd73cbe0e532176ce007d5f696c29
risvh <67486979+risvh@users.noreply.github.com>
2022-06-27 00:00:43 +0800

    Redesign the Settings page

--- gameSource/SettingsPage.cpp
+++ gameSource/SettingsPage.cpp
@@ -24,33 +24,46 @@ extern float musicLoudness;
 
 
 SettingsPage::SettingsPage()
-        : mInfoSeeds( mainFont, 542, -150, "?" ),
-		  mBackButton( mainFont, -542, -280, translate( "backButton" ) ),
+        : mBackground( "background.tga", 0.75f ),
+          
+          // Left Pane
+          mRestartButton( mainFont, 360, -272, translate( "restartButton" ) ),
+          
+          mGameplayButton( mainFont, -452.5, 208, "GAMEPLAY" ),
+          mControlButton( mainFont, -452.5, 112, "CONTROL" ),
+          mScreenButton( mainFont, -452.5, 16, "SCREEN" ),
+          mSoundButton( mainFont, -452.5, -80, "SOUND" ),
+          mBackButton( mainFont, -452.5, -272, translate( "backButton" ) ),
+          
           mEditAccountButton( mainFont, -463, 129, translate( "editAccount" ) ),
-          mRestartButton( mainFont, 128, 128, translate( "restartButton" ) ),
+
+          // Gameplay
+		  mEnableFOVBox( 561, 128, 4 ),
+		  mEnableCenterCameraBox( 561, 52, 4 ),
+          mEnableNudeBox( -335, 148, 4 ),
+          
+          // Control
+		  mEnableKActionsBox( 561, 90, 4 ),
+          mCursorScaleSlider( mainFont, 297, 155, 4, 200, 30,
+                                       1.0, 10.0, 
+                                       translate( "scale" ) ),
+          
+          // Screen
           mRedetectButton( mainFont, 153, 249, translate( "redetectButton" ) ),
           mFullscreenBox( 0, 128, 4 ),
           mBorderlessBox( 0, 168, 4 ),
-          mEnableNudeBox( -335, 148, 4 ),
-		  mEnableFOVBox( 561, 128, 3),
-		  mEnableKActionsBox( 561, 90, 3),
-		  mEnableCenterCameraBox( 561, 52, 3),
+          
+          // Sound
           mMusicLoudnessSlider( mainFont, 0, 40, 4, 200, 30,
                                 0.0, 1.0, 
                                 translate( "musicLoudness" ) ),
           mSoundEffectsLoudnessSlider( mainFont, 0, -48, 4, 200, 30,
                                        0.0, 1.0, 
-                                       translate( "soundLoudness" ) ),
-          mSpawnSeed( mainFont, 226, -150, 14, false, 
-                                     translate( "spawnSeed" ),
-                                     NULL,
-                                     // forbid spaces
-                                     " " ),
-          mCursorScaleSlider( mainFont, 297, 155, 4, 200, 30,
-                                       1.0, 10.0, 
-                                       translate( "scale" ) ) {
+                                       translate( "soundLoudness" ) ) {
                             
 
+    
+    addComponent( &mBackground );
     
     const char *choiceList[3] = { translate( "system" ),
                                   translate( "drawn" ),
@@ -68,20 +81,43 @@ SettingsPage::SettingsPage()
 
     mCursorScaleSlider.toggleField( false );
 
-
-	setButtonStyle( &mInfoSeeds );
+    setButtonStyle( &mGameplayButton );
+    mGameplayButton.setSize( 175, 60 );
+    addComponent( &mGameplayButton );
+    mGameplayButton.addActionListener( this );
+    
+    setButtonStyle( &mControlButton );
+    mControlButton.setSize( 175, 60 );
+    addComponent( &mControlButton );
+    mControlButton.addActionListener( this );
+    
+    setButtonStyle( &mScreenButton );
+    mScreenButton.setSize( 175, 60 );
+    addComponent( &mScreenButton );
+    mScreenButton.addActionListener( this );
+    
+    setButtonStyle( &mSoundButton );
+    mSoundButton.setSize( 175, 60 );
+    addComponent( &mSoundButton );
+    mSoundButton.addActionListener( this );
+        
     setButtonStyle( &mBackButton );
-    setButtonStyle( &mEditAccountButton );
-    setButtonStyle( &mRestartButton );
-    setButtonStyle( &mRedetectButton );
-
-	//addComponent( &mInfoSeeds );
-	//mInfoSeeds.addActionListener( this );
-
+    mBackButton.setSize( 175, 60 );
     addComponent( &mBackButton );
     mBackButton.addActionListener( this );
 
-    addComponent( &mEditAccountButton );
+    setButtonStyle( &mRestartButton );
+    mRestartButton.setSize( 175, 60 );
+    addComponent( &mRestartButton );
+    mRestartButton.addActionListener( this );
+    mRestartButton.setVisible( false );
+
+    setButtonStyle( &mRedetectButton );
+    addComponent( &mRedetectButton );
+    mRedetectButton.addActionListener( this );
+    
+    setButtonStyle( &mEditAccountButton );
+    // addComponent( &mEditAccountButton );
     mEditAccountButton.addActionListener( this );
 
     addComponent( &mFullscreenBox );
@@ -101,16 +137,28 @@ SettingsPage::SettingsPage()
 	
 	addComponent( &mEnableCenterCameraBox );
     mEnableCenterCameraBox.addActionListener( this );
-
-    addComponent( &mRestartButton );
-    mRestartButton.addActionListener( this );
     
-    addComponent( &mRedetectButton );
-    mRedetectButton.addActionListener( this );
-
-    //addComponent( &mSpawnSeed );
     
-    mRestartButton.setVisible( false );
+    
+    mRestartButton.setCursorTip( "RESTART THE GAME" );
+    
+    mGameplayButton.setCursorTip( "GAMEPLAY SETTINGS" );
+    mControlButton.setCursorTip( "CONTROL SETTINGS" );
+    mScreenButton.setCursorTip( "SCREEN SETTINGS" );
+    mSoundButton.setCursorTip( "SOUND SETTINGS" );
+    mBackButton.setCursorTip( "BACK TO LOGIN PAGE" );
+    
+    mEnableFOVBox.setCursorTip( "ENABLE ZOOM-IN AND ZOOM-OUT WITH MOUSE WHEEL SCROLLING" );
+    mEnableCenterCameraBox.setCursorTip( "ALWAYS CENTER THE CAMERA VIEW ON YOUR CHARACTER" );
+    mEnableNudeBox.setCursorTip( "ENABLE NUDITY" );
+    
+    mEnableKActionsBox.setCursorTip( "ENABLE WASD MOVEMENT AND ACTION" );
+    mCursorModeSet->setCursorTip( "USE DRAWN CURSOR FOR ULTRAWIDE SCREEN" );
+    
+    mRedetectButton.setCursorTip( "RESTART THE GAME TO REDETECT FRAME RATE" );
+    mFullscreenBox.setCursorTip( "TOGGLE BETWEEN FULLSCREEN AND WINDOWED MODE" );
+    mBorderlessBox.setCursorTip( "ALLOW CURSOR TO MOVE OUTSIDE THE GAME WINDOW" );
+
     
     mOldFullscreenSetting = 
         SettingsManager::getIntSetting( "fullscreen", 1 );
@@ -149,6 +197,8 @@ SettingsPage::SettingsPage()
 	
 	mEnableCenterCameraBox.setToggled( mEnableCenterCameraSetting );
     
+    mPage = 0;
+    
 
     addComponent( &mMusicLoudnessSlider );
     addComponent( &mSoundEffectsLoudnessSlider );
@@ -171,24 +221,10 @@ SettingsPage::~SettingsPage() {
 
 void SettingsPage::actionPerformed( GUIComponent *inTarget ) {
     if( inTarget == &mBackButton ) {
-        
-        char *seedList = mSpawnSeed.getAndUpdateList();
-        
-        SettingsManager::setSetting( "spawnSeed", seedList );
-        delete [] seedList;
-        
         setSignal( "back" );
         setMusicLoudness( 0 );
         }
-	else if( inTarget == &mInfoSeeds ) {
-		 char *url = strdup("https://twohoursonelife.fandom.com/wiki/Spawn_seeds");
-		 
-		 if( strcmp( url, "" ) != 0 ) {
-			 launchURL( url );
-			}
-		}
     else if( inTarget == &mEditAccountButton ) {
-        
         setSignal( "editAccount" );
         setMusicLoudness( 0 );
         }
@@ -196,24 +232,16 @@ void SettingsPage::actionPerformed( GUIComponent *inTarget ) {
         int newSetting = mFullscreenBox.getToggled();
         
         SettingsManager::setSetting( "fullscreen", newSetting );
-        
-        mRestartButton.setVisible( mOldFullscreenSetting != newSetting );
-        
-        mBorderlessBox.setVisible( newSetting );
         }
     else if( inTarget == &mBorderlessBox ) {
         int newSetting = mBorderlessBox.getToggled();
         
         SettingsManager::setSetting( "borderless", newSetting );
-        
-        mRestartButton.setVisible( mOldBorderlessSetting != newSetting );
         }
 	else if( inTarget == &mEnableNudeBox ) {
         int newSetting = mEnableNudeBox.getToggled();
         
         SettingsManager::setSetting( "nudeEnabled", newSetting );
-        
-        mRestartButton.setVisible( mEnableNudeSetting != newSetting );
         }
 	else if( inTarget == &mEnableFOVBox ) {
         int newSetting = mEnableFOVBox.getToggled();
@@ -291,17 +319,30 @@ void SettingsPage::actionPerformed( GUIComponent *inTarget ) {
     else if( inTarget == mCursorModeSet ) {
         setCursorMode( mCursorModeSet->getSelectedItem() );
         
-        int mode = getCursorMode();
+        // int mode = getCursorMode();
         
-        mCursorScaleSlider.setVisible( mode > 0 );
+        // mCursorScaleSlider.setVisible( mode > 0 );
         
         mCursorScaleSlider.setValue( getEmulatedCursorScale() );
         }
     else if( inTarget == &mCursorScaleSlider ) {
         setEmulatedCursorScale( mCursorScaleSlider.getValue() );
         }
+    else if( inTarget == &mGameplayButton ) {
+        mPage = 0;
+        }
+    else if( inTarget == &mControlButton ) {
+        mPage = 1;
+        }
+    else if( inTarget == &mScreenButton ) {
+        mPage = 2;
+        }
+    else if( inTarget == &mSoundButton ) {
+        mPage = 3;
+        }
     
-    
+    checkRestartRequired();
+    updatePage();
     }
 
 
@@ -312,105 +353,122 @@ void SettingsPage::draw( doublePair inViewCenter,
                          double inViewSize ) {
     setDrawColor( 1, 1, 1, 1 );
     
-    doublePair pos = mFullscreenBox.getPosition();
-    
-    pos.x -= 30;
-    pos.y -= 2;
-    
-    mainFont->drawString( translate( "fullscreen" ), pos, alignRight );
-
-
-    if( mBorderlessBox.isVisible() ) {
-        pos = mBorderlessBox.getPosition();
-    
+    if( mFullscreenBox.isVisible() ) {
+        doublePair pos = mFullscreenBox.getPosition();
+        
         pos.x -= 30;
         pos.y -= 2;
         
-        mainFont->drawString( translate( "borderless" ), pos, alignRight );
-        }
-    
-
-    pos = mFullscreenBox.getPosition();
-    
-    pos.y += 96;
-    pos.x -= 16;
-    
-    if( getCountingOnVsync() ) {
-        mainFont->drawString( translate( "vsyncYes" ), pos, alignLeft );
-        }
-    else {
-        mainFont->drawString( translate( "vsyncNo" ), pos, alignLeft );
-        }
-    
-    pos.y += 44;
-
-    char *fpsString = autoSprintf( "%d", targetFramesPerSecond );
-    
-    mainFont->drawString( fpsString, pos, alignLeft );
-    delete [] fpsString;
+        mainFont->drawString( translate( "fullscreen" ), pos, alignRight );
 
 
-    pos.y += 44;
-
-    char *currentFPSString = autoSprintf( "%.2f", getRecentFrameRate() );
-    
-    mainFont->drawString( currentFPSString, pos, alignLeft );
-    delete [] currentFPSString;
-    
-
-    pos = mFullscreenBox.getPosition();
-    pos.x -= 30;
-
-    pos.y += 96;
-    mainFont->drawString( translate( "vsyncOn" ), pos, alignRight );
-    pos.y += 44;
-    mainFont->drawString( translate( "targetFPS" ), pos, alignRight );
-    pos.y += 44;
-    mainFont->drawString( translate( "currentFPS" ), pos, alignRight );
-
-
-    pos = mEnableNudeBox.getPosition();
-    
-    pos.x -= 30;
-    pos.y -= 2;
-
-    mainFont->drawString( "Enable Nudity", pos, alignRight );
-	
-	pos = mEnableFOVBox.getPosition();
-    
-    pos.x -= 30;
-    pos.y -= 2;
-
-    mainFont->drawString( "Enable FOV", pos, alignRight );
-	
-	pos = mEnableKActionsBox.getPosition();
-    
-    pos.x -= 30;
-    pos.y -= 2;
-
-    mainFont->drawString( "Keyboard Actions", pos, alignRight );
-	
-	pos = mEnableCenterCameraBox.getPosition();
-    
-    pos.x -= 30;
-    pos.y -= 2;
-
-    mainFont->drawString( "Center Camera", pos, alignRight );
-
-
-    pos = mCursorModeSet->getPosition();
-    
-    pos.y += 37;
-    mainFont->drawString( translate( "cursor"), pos, alignRight );
-    
-    if( mCursorScaleSlider.isVisible() ) {
+        if( mBorderlessBox.isVisible() ) {
+            pos = mBorderlessBox.getPosition();
         
-        pos = mCursorScaleSlider.getPosition();
+            pos.x -= 30;
+            pos.y -= 2;
+            
+            mainFont->drawString( translate( "borderless" ), pos, alignRight );
+            }
         
-        pos.x += 72;
+
+        pos = mFullscreenBox.getPosition();
+        
+        pos.y += 52;
+        pos.x -= 16;
+        
+        if( getCountingOnVsync() ) {
+            mainFont->drawString( translate( "vsyncYes" ), pos, alignLeft );
+            }
+        else {
+            mainFont->drawString( translate( "vsyncNo" ), pos, alignLeft );
+            }
+        
+        pos.y += 52;
+
+        char *fpsString = autoSprintf( "%d", targetFramesPerSecond );
+        
+        mainFont->drawString( fpsString, pos, alignLeft );
+        delete [] fpsString;
+
+
+        pos.y += 52;
+
+        char *currentFPSString = autoSprintf( "%.2f", getRecentFrameRate() );
+        
+        mainFont->drawString( currentFPSString, pos, alignLeft );
+        delete [] currentFPSString;
+        
+
+        pos = mFullscreenBox.getPosition();
+        pos.x -= 30;
+
+        pos.y += 52;
+        mainFont->drawString( translate( "vsyncOn" ), pos, alignRight );
+        pos.y += 52;
+        mainFont->drawString( translate( "targetFPS" ), pos, alignRight );
+        pos.y += 52;
+        mainFont->drawString( translate( "currentFPS" ), pos, alignRight );
+        }
+
+
+    if( mEnableNudeBox.isVisible() ) {
+        doublePair pos = mEnableNudeBox.getPosition();
+        
+        pos.x -= 30;
         pos.y -= 2;
+
+        mainFont->drawString( "ENABLE NUDITY", pos, alignRight );
+        }
+	
+    if( mEnableFOVBox.isVisible() ) {
+        doublePair pos = mEnableFOVBox.getPosition();
         
-        mainFont->drawString( translate( "scale"), pos, alignRight );
+        pos.x -= 30;
+        pos.y -= 2;
+
+        mainFont->drawString( "ENABLE ZOOM", pos, alignRight );
+        }
+	
+    if( mEnableKActionsBox.isVisible() ) {
+        doublePair pos = mEnableKActionsBox.getPosition();
+        
+        pos.x -= 30;
+        pos.y -= 2;
+
+        mainFont->drawString( "KEYBOARD ACTIONS:", pos, alignRight );
+        }
+	
+    if( mEnableCenterCameraBox.isVisible() ) {
+        doublePair pos = mEnableCenterCameraBox.getPosition();
+        
+        pos.x -= 30;
+        pos.y -= 2;
+
+        mainFont->drawString( "ALWAYS CENTER CAMERA", pos, alignRight );
+        }
+
+
+    if( mCursorModeSet->isVisible() ) {
+        
+        double xPos = mEnableCenterCameraBox.getPosition().x - 30;
+        
+        doublePair pos = mCursorModeSet->getPosition();
+        
+        pos.x = xPos;
+        pos.y += 37;
+        
+        mainFont->drawString( translate( "cursor"), pos, alignRight );
+        
+        if( mCursorScaleSlider.isVisible() ) {
+            
+            pos = mCursorScaleSlider.getPosition();
+            
+            pos.x = xPos;
+            pos.y -= 2;
+            
+            mainFont->drawString( "DRAWN CURSOR SIZE:", pos, alignRight );
+            }
         }
     
     }
@@ -435,18 +493,7 @@ void SettingsPage::makeActive( char inFresh ) {
         
         mCursorModeSet->setSelectedItem( mode );
         
-        mCursorScaleSlider.setVisible( mode > 0 );
-        
         mCursorScaleSlider.setValue( getEmulatedCursorScale() );
-
-
-        char *seed = 
-            SettingsManager::getSettingContents( "spawnSeed",
-                                               "" );
-        
-        mSpawnSeed.setList( seed );
-        
-        delete [] seed;
         
 
 
@@ -480,6 +527,8 @@ void SettingsPage::makeActive( char inFresh ) {
         else {
             mEditAccountButton.setVisible( false );
             }
+            
+        updatePage();
 
         }
     }
@@ -489,3 +538,64 @@ void SettingsPage::makeActive( char inFresh ) {
 void SettingsPage::makeNotActive() {
     }
 
+
+void SettingsPage::updatePage() {
+
+    doublePair pos = { 0, 0 };
+    double lineSpacing = 52;
+    
+    mEnableFOVBox.setPosition( 0, lineSpacing );
+    mEnableCenterCameraBox.setPosition( 0, 0 );
+    mEnableNudeBox.setPosition( 0, -lineSpacing );
+    
+    mEnableKActionsBox.setPosition( 0, lineSpacing * 2 );
+    mCursorModeSet->setPosition( 0, 0 );
+    mCursorScaleSlider.setPosition( -80, -lineSpacing * 3 );
+    
+    mMusicLoudnessSlider.setPosition( 0, lineSpacing / 2 );
+    mSoundEffectsLoudnessSlider.setPosition( 0, - lineSpacing / 2 );
+    
+    mFullscreenBox.setPosition( 0, -lineSpacing );
+    mBorderlessBox.setPosition( 0, -lineSpacing * 2 );
+    mRedetectButton.setPosition( 160, lineSpacing * 2 );
+    mRedetectButton.setPadding( 8, 4 );
+    
+    mEnableFOVBox.setVisible( mPage == 0 );
+    mEnableCenterCameraBox.setVisible( mPage == 0 );
+    mEnableNudeBox.setVisible( mPage == 0 );
+
+    mEnableKActionsBox.setVisible( mPage == 1 );
+    mCursorModeSet->setVisible( mPage == 1 );
+    int mode = getCursorMode();
+    mCursorScaleSlider.setVisible( mode > 0 && mCursorModeSet->isVisible() );
+    
+
+    mFullscreenBox.setVisible( mPage == 2 );
+    mBorderlessBox.setVisible( mPage == 2 && mFullscreenBox.getToggled() );
+    mRedetectButton.setVisible( mPage == 2 );
+
+    mMusicLoudnessSlider.setVisible( mPage == 3 );
+    mSoundEffectsLoudnessSlider.setVisible( mPage == 3 );
+
+
+    mGameplayButton.setActive( mPage != 0 );
+    mControlButton.setActive( mPage != 1 );
+    mScreenButton.setActive( mPage != 2 );
+    mSoundButton.setActive( mPage != 3 );
+  
+
+}
+
+bool SettingsPage::checkRestartRequired() {
+    if( mOldFullscreenSetting != mFullscreenBox.getToggled() ||
+        mOldBorderlessSetting != mBorderlessBox.getToggled() ||
+        mEnableNudeSetting != mEnableNudeBox.getToggled()
+        ) {
+        setStatusDirect( "RESTART REQUIRED##FOR NEW SETTINGS TO TAKE EFFECT", true );
+        mRestartButton.setVisible( true );
+        }
+    else {
+        setStatus( NULL, false );
+        mRestartButton.setVisible( false );
+        }
+    }
