7dfc8b30aa4e3f1f85b0879b33884e1390a311ae
Jason Rohrer <jasonrohrer@fastmail.fm>
2023-07-10 17:43:19 -0400

    Finally found source of disconnect-on-death bug.  When client receives its own X X death message, it instantly closes the socket to the server.  However, later code in the remainder of that client step sometimes sends messages to the server (in the case of FLIP, for example).  When this happens, the send was failing, and the disconnection signal was being raised, overriding the you-died signal.  Fixed by simply not sending a message through the socket if it's already closed.  Thanks Agne for sending a log catching the problem.  Fixes #852

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -1076,6 +1076,12 @@ void LivingLifePage::sendToServerSocket( char *inMessage ) {
     timeLastMessageSent = game_getCurrentTime();
     
     printf( "Sending message to server: %s\n", inMessage );
+
+    if( mServerSocket == -1 ) {
+        printf( "Server socket already closed, skipping sending message: %s\n",
+                inMessage );
+        return;
+        }
     
     replaceLastMessageSent( stringDuplicate( inMessage ) );    
 
@@ -3283,6 +3289,7 @@ LivingLifePage::~LivingLifePage() {
     
     if( mServerSocket != -1 ) {
         closeSocket( mServerSocket );
+        mServerSocket = -1;
         }
     
     for( int j=0; j<2; j++ ) {
