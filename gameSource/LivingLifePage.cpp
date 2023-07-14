62b4328c241bf3270ed7edb26e6ea9005d4a0914
Jason Rohrer <jasonrohrer@fastmail.fm>
2023-07-14 13:06:34 -0400

    Fixed so that creation sound's initalOnly is respected for floor objects.

--- gameSource/LivingLifePage.cpp
+++ gameSource/LivingLifePage.cpp
@@ -15324,8 +15324,10 @@ void LivingLifePage::step() {
                             // floor changed
                             
                             ObjectRecord *obj = getObject( floorID );
-                            if( obj->creationSound.numSubSounds > 0 ) {    
-                                    
+                            if( obj->creationSound.numSubSounds > 0 &&
+                                shouldCreationSoundPlay( oldFloor, 
+                                                         floorID ) ) {    
+                                
                                 playSound( obj->creationSound,
                                            getVectorFromCamera( x, y ) );
                                 }
